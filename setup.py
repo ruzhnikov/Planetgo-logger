10564db66ec950cc07b8ebce43d69afc276f7830
Alexander Ruzhnikov <ruzhnikov85@gmail.com>
2018-05-15 21:45:54 +0300

    * keep working on logger file * added setup.py

---
+++ setup.py
@@ -0,0 +1,20 @@
+
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+import logger as planetgo_logger
+
+setup(
+    name="planetgo_logger",
+    version=planetgo_logger.__version__,
+    packages=find_packages(exclude=['tests']),
+    author="Alexander Ruzhnikov",
+    author_email="ruzhnikov85@gmail.com",
+    license="MIT",
+    python_requires='>=3',
+    description="Planetgo logger",
+    long_description="""
+        A simple logger for using in other kinds of planetgo site
+        """
+)
